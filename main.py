628a533323b8e6a132796fc5c441a4077984b8c3
otoz-977 <49078425+otoz-977@users.noreply.github.com>
2023-05-31 00:17:03 -0700

    dB integration
    muestra tweets y emepeze a crear tweets nuevos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template,jsonify
 from datetime import datetime
 from objects import Tweet, User
 import mysql.connector
+from datetime import datetime
 import json
 
 app = Flask(__name__)
@@ -12,29 +13,14 @@ db = mysql.connector.connect(
     password="",
     database="tweetdisarq"  # Replace with your actual database name
 )
+
 @app.route('/')
 def hello_world():
     #return 'Hello, World!  check out this tweet {}'.format(create_tweet())
     return render_template('Home.html')
-
-
 @app.route('/create_tweet', methods=['POST'])
 def create_tweet():
-    # Get the text of the tweet from the request data
-    text = "Hola, como estan este es un tweet"##request.form['text']
-
-    # Set the default author and timestamp
-    author = "testAuthor"
-    timestamp = datetime.now()
-
-    # Create a new instance of the Tweet class with the provided text, author, and timestamp
-    tweet = Tweet(text, author, timestamp)
-
-    # Do something with the new tweet instance (e.g. store it in a database)
-    # ...
-
     return render_template('newTweet.html')
-
 
 @app.route('/board', methods=['POST'])
 def board():
@@ -47,7 +33,6 @@ def board():
     cursor.execute(query)
     data = cursor.fetchall()
     return render_template('DashBoard.html', data=data)
-
 @app.route('/details', methods=['POST'])
 def details():
     thread = load_tweets_from_json()
@@ -62,6 +47,27 @@ def load_tweets_from_json():
         tweets = json.load(file)
     return tweets
 
+
+def insert_tweet(text):
+    _datetime = datetime.now()
+    # Convert date and time to strings
+    cur_fecha = _datetime.strftime('%Y-%m-%d')
+    cur_hora = _datetime.strftime('%H:%M:%S')
+    cursor = db.cursor()
+    query = """
+    INSERT INTO tweet (fecha, hora, author, text)
+    VALUES (%s, %s, %s, %s)
+    """
+    values = (cur_fecha, cur_hora, "ElOtoMotto", text)  # Replace with appropriate values for fecha, hora, and author
+    cursor.execute(query, values)
+    db.commit()
+    return "Tweet inserted successfully"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    tweet_text = request.form['tweet_text']
+    insert_tweet(tweet_text)
+    return "Tweet generated and inserted into the database successfully"
 
 if __name__ == '__main__':
     app.run()
